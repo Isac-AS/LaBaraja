3b601d2ae23e0dc02a75f33cd38501f08c68dfe0
isac <isac.as0001@gmail.com>
2022-03-21 13:13:56 +0000

    Application

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 """ https://github.com/techwithtim/Flask-Web-App-Tutorial/tree/main/website will be used as an starting point for the project
 """
+from email.mime import application
 from website import create_app
 
 app = create_app()
+application = app
 
 if __name__ == '__main__':
     app.run(debug=True)
